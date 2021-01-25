9d1b2052e9f8448d3621c5bba6160270551fb34f
Luan Pinho <luanpinhoo@gmail.com>
2021-01-25 12:42:58 -0400

    feat(icons): added skills

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -4,9 +4,9 @@ import DefaultComponent from '../../Components/DefaultComponent'
 import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import MailIcon from '@material-ui/icons/Mail';
-
 import GitHubIcon from '@material-ui/icons/GitHub';
-
+import { CssIcon, TsIcon, DockerIcon, JsIcon, MysqlIcon, NodeIcon, PsqlIcon, ReactIcon, ReduxtIcon } from '../../Components/Icons/icons';
+ 
 export default function Home() {
     return(
         <DefaultComponent title="Bem vindo">
@@ -14,6 +14,18 @@ export default function Home() {
             <section id="primary">
                 <h1 id="name">Wanderson Pinho</h1>
                 <h2>FullStack Developer</h2>
+            </section>
+            <section id="three">
+                <h2>Skills</h2>
+                    <JsIcon/>
+                    <ReactIcon />
+                    <NodeIcon />
+                    <ReduxtIcon />
+                    <CssIcon />
+                    <TsIcon />
+                    <DockerIcon/>
+                    <MysqlIcon />
+                    <PsqlIcon />
             </section>
             <section id="secondary">
                 <div onClick={() => window.location.href= "https://api.whatsapp.com/send?phone=5567996576515"}>
